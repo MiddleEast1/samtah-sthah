d4871adabb991d308141cd1a9ac0b79618aa5776
MiddleEast <sami.0554539155@gmail.com>
2026-08-04 01:55:47 +0300

    اصدار 2

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,17 @@
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
-import {defineConfig} from 'vite';
+import { defineConfig } from 'vite';
 
-export default defineConfig(() => {
-  return {
-    base: './',
-    plugins: [react(), tailwindcss()],
-    resolve: {
-      alias: {
-        '@': path.resolve(__dirname, '.'),
-      },
+export default defineConfig({
+  base: '/samtah-sthah/',
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, '.'),
     },
+  },
+});
     server: {
       host: '0.0.0.0',
       port: 3000,
